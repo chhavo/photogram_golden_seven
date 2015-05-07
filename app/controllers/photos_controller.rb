858cc4824f108cbdfe4e14d14d9fbf57e3ba6a02
chhavo <cadtani@gmail.com>
2015-05-07 17:01:00 -0500

    fixed_the_show_part

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,24 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
+  def show
 
+    @list_of_photos = Photo.all
+    @id = params[:id]
+    @temp = 0
+
+  @show_photo = Photo.find_by({:id=> @id})
+
+
+    # @list_of_photos.each do |photo|
+    #   if photo.id = @id
+    #     @show_photo = photo.source
+    #     @temp = photo.id
+    #   end
+    # end
+
+  end
 end
